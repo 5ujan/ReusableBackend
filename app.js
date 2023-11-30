78e2d935c49ed7ec3fbc25d24b6aa2e321ce5c10
Sujan Baskota <sujan.baskota77@gmail.com>
2023-11-30 06:26:05 +0545

    goauth

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ const express = require('express')
 const app= express();
 
 //middlewares
+const passport = require('passport')
 const connectDB=require('./db/connect')
 const {errorHandler} = require('./middleware/handleErrors')
 const authRouter= require("./routes/auth");
 const mainRouter= require("./routes/main");
 const authenticate = require('./middleware/authentication');
-
+require('./g-auth')
 
 app.use(express.json())
-app.use('/',  authRouter)         //login or register, doesn't require authentication
-app.use('/', authenticate, mainRouter)
+app.use('/auth',  authRouter)         //login or register, doesn't require authentication
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/auth/google/failure",
+  })
+);
+
+app.use('/dashboard',  mainRouter)
 
 
 app.use(errorHandler);
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,38 +1,44 @@
-const User = require("../models/User")
-const {BadRequestError, NotFoundError, UnauthenticatedError} = require('../middleware/handleErrors')
 
 
-const register = async(req, res, next)=>{
-    const {name, email, password, isOrg} = req.body
-    const alreadyExists = await User.findOne({email})
-    if(alreadyExists){
+const auth = async (req, res, next) => {
+  res.send('<a href="/auth/google/" >Continue with google</a>');
+  console.log("Auth/get");
+};
+const failure = async (req, res, next) => {
+  res.send('<p>Google login failed<p></p><a href="/auth/" >Go back?</a>');
+};
+
+module.exports= {auth, failure}
+
+// const register = async(req, res, next)=>{
+//     const {name, email, password, isOrg} = req.body
+//     const alreadyExists = await User.findOne({email})
+//     if(alreadyExists){
         
-        next(new BadRequestError("Account with the email already exists"))
+//         next(new BadRequestError("Account with the email already exists"))
          
-    }else{
-        const user= await User.create({name, email, password, isOrg})
-        const token= await user.createJWT()
+//     }else{
+//         const user= await User.create({name, email, password, isOrg})
+//         const token= await user.createJWT()
 
-        res.json({name: user.name, token})
-    }
-}
+//         res.json({name: user.name, token})
+//     }
+// }
 
-const login = async(req, res, next)=>{
-    const  {email, password } = req.body
-    const user = await User.findOne({email})
-    if(!user){
+// const login = async(req, res, next)=>{
+//     const  {email, password } = req.body
+//     const user = await User.findOne({email})
+//     if(!user){
         
-        next(new NotFoundError("Account with the email doesn't exists"));
+//         next(new NotFoundError("Account with the email doesn't exists"));
 
-    }else{
-        if(!user.verifyPassword(password)){
-            next(new UnauthenticatedError("Wrong password"))
-        }
-        const token = await user.createJWT()
-        res.json({name: user.name, token})
-    }
-}
+//     }else{
+//         if(!user.verifyPassword(password)){
+//             next(new UnauthenticatedError("Wrong password"))
+//         }
+//         const token = await user.createJWT()
+//         res.json({name: user.name, token})
+//     }
+// }
 
 
-
-module.exports = {register, login}
