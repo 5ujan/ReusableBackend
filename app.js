6c29732ce6d715556462301100a9b2b4e4d3a126
praharshaAdhikari <114891918+praharshaAdhikari@users.noreply.github.com>
2023-12-15 09:40:57 +0545

    minor

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ const mainRouter= require("./routes/main");
 const userRouter= require("./routes/user");
 const authenticate = require('./middleware/authentication');
 
-
-app.use(express.json({limit: '25mb'}));
+app.use(express.json())
 app.use('/',  authRouter)         //login or register, doesn't require authentication
 app.use('/', authenticate, mainRouter, userRouter)
 
@@ -21,15 +20,13 @@ app.use(errorHandler);
 const start=async()=>{
     try {
         const port = 5000
-        await connectDB(process.env.MONGO_URI)
+        await connectDB(process.env.MONGO_URI);
         app.listen(port, ()=>{
-            console.log("server listening on port "+port)
-            
+            console.log(`Server listening on port ${port}.`)
         })
     }
     catch (error) {
         errorHandler(error);
     }
 }
-    
 start()
--- db/connect.js
+++ db/connect.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 
 // const connectionString = process.env.MONGO_URI;
 const connectDB = (url) =>{
-
-    return mongoose.connect(url,
+    const conn = mongoose.connect(url,
         {
             useNewUrlParser: true,
             useUnifiedTopology: true
-        })
+        });
+        console.log(`Database connected.`)
+        return conn;
 }
 
 module.exports = connectDB
